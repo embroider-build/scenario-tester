31f190297fc1b0814e4f1c00fcfde77600082f9d
Edward Faulkner <ef@alum.mit.edu>
2021-03-29 01:46:49 -0400

    clear outdir

--- output.ts
+++ output.ts
@@ -1,3 +1,4 @@
+import { removeSync } from 'fs-extra';
 import { list, ListParams } from './list';
 
 export interface OutputParams extends ListParams {
@@ -10,6 +11,7 @@ export async function output(params: OutputParams) {
   for (let scenario of scenarios) {
     if (scenario.name.indexOf(params.scenario) !== -1) {
       process.stdout.write(`Found scenario ${scenario.name}\n`);
+      removeSync(params.outdir);
       await scenario.prepare(params.outdir);
       process.stdout.write(`Wrote successfully to ${params.outdir}\n`);
       return;
